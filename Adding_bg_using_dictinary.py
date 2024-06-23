a609f09cf4ed36ab79c8b90d4f667f736b04897b
navdeepsingh160 <118738353+navdeepsingh160@users.noreply.github.com>
2024-06-23 10:24:13 +0530

    Add files via upload

---
+++ Adding_bg_using_dictinary.py
@@ -0,0 +1,15 @@
+import tkinter
+
+import random
+
+Key = “Colours”
+Value = list of colour 
+
+list of color = ["maroon1","lawn green","magenta2","purple1","springgreen2","chocolate1",
+"deep pink","cyan"]
+
+random.randint(1,9)
+print()
+
+root.configure(background =variable_name_that_holds_the_random_color_picked)
+

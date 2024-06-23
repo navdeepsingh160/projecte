a94681e9af38ddaa22b627db740a7394a4abcf2a
navdeepsingh160 <118738353+navdeepsingh160@users.noreply.github.com>
2024-06-23 10:24:26 +0530

    Add files via upload

---
+++ Sales_application_project.py
@@ -0,0 +1,13 @@
+import tkinter
+root = Tk()
+
+label_month ["text"] = "Months: " + str(month)
+
+Months: ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+
+
+
+label_profit["text"] = "Profits:" +str(profits)
+
+Profits:(20000, 45000, 78000, 97000, 12000, 456000, 65000, 54000, 10000, 30000, 70000, 90000)
+
